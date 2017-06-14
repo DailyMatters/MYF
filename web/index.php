24e4025f389f393c979e79edb29e95f7ce3101cf
Claudio Ribeiro <cribeiro@ventureoak.com>
2017-06-14 11:49:00 +0100

    Basic DI component installed and working

--- web/index.php
+++ web/index.php
@@ -3,28 +3,13 @@
 // framework/index.php
 require_once './vendor/autoload.php';
 
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\Routing;
+
+$routes = include './src/app.php';
+$sc = include './src/container.php';
 
 $request = Request::createFromGlobals();
-$requestStack = new RequestStack();
-$routes = include './src/app.php';
 
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+$response = $sc->get('framework')->handle($request);
 
-$controllerResolver = new HttpKernel\Controller\ControllerResolver();
-$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
-
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
-$dispatcher->addSubscriber(new Simplex\StringResponseListener());
-
-$framework = new Simplex\Framework($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
-
-$response = $framework->handle($request);
 $response->send();
